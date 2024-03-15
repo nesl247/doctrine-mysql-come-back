4a7f0c37de1147c9e24a9e62b81fbe4db803470f
Alexander Kim <alex@kim.family>
2024-03-15 13:50:06 -0400

    feat: add postgres support

--- src/Connections/PrimaryReadReplicaConnection.php
+++ src/Connections/PrimaryReadReplicaConnection.php
@@ -25,6 +25,6 @@ class PrimaryReadReplicaConnection extends \Doctrine\DBAL\Connections\PrimaryRea
             unset($params['primary']['driverOptions']['x_reconnect_attempts']);
         }
 
-        self::__traitConstruct($params, $driver, $config, $eventManager);
+        $this->__traitConstruct($params, $driver, $config, $eventManager);
     }
 }
